116629e6e56a3a4598eea789bfe3110431f0af70
Jenifer Tabita Ciuciu-Kiss <jenifer.girl.98@gmail.com>
2021-11-07 17:15:55 +0100

    Fixing the location of the Java file

--- VehicleTelematics/src/main/java/master/VehicleTelematics.java
+++ VehicleTelematics/src/main/java/master/VehicleTelematics.java
@@ -39,39 +39,35 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+
 
 import java.io.*;
+import java.util.*;
 import java.lang.Integer;
 import java.lang.String;
 
 
 public class VehicleTelematics {
 
-	//
-	//	Program
-	//
-
 	public static void main(String[] args) throws Exception {
-
-		//final ParameterTool params = ParameterTool.fromArgs(args);
 
 		// input file path
 		String inputFilePath = args[0];
-
-		System.out.println(inputFilePath);
 		// output file path
 		String outputFilePath = args[1];		
-
 
 		// set up the execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // get input data reading the text file from given input path
         DataStream<String> text = env.readTextFile(inputFilePath).setParallelism(1);
-
+		
 		// parse input to Tuple8
-		SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> s1 = text.
+		SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> input = text.
 		map(new MapFunction<String, Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
 			public Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> map(String in) throws Exception{
 				String[] fieldArray = in.split(",");
@@ -88,36 +84,66 @@ public class VehicleTelematics {
 			}
 		}).setParallelism(1);
 
-		System.out.println(s1.print());
+		// Speed radar
 
-		// VehicleID as key
-		KeyedStream<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple> s2 = s1.keyBy(1);
-
-		// filter the rows with speed over 90
-		SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> filtered;
-
-		filtered = s1.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
+		SingleOutputStreamOperator speedFines = input.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
 			@Override
-			public boolean filter(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> data) throws Exception {
-				return (data.f2 > 90);
+			public boolean filter(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> d) throws Exception {
+				return (d.f2 > 90);
 			}
-		});
-
-		// select the needed columns to return
-		SingleOutputStreamOperator<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> speedFines = filtered.map(
+		}).map(
 			new MapFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>,
 				Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>>() {
 					public Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> 
-					map(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> data) throws Exception{
-						Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> res = new Tuple6<> (data.f0, data.f1, data.f3, data.f6, data.f5, data.f2);
+					map(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> d) throws Exception{
+						Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> res = new Tuple6<> (d.f0, d.f1, d.f3, d.f6, d.f5, d.f2);
 						return res;
 					}});
 		
 		// print the results for speedfines
-		System.out.println("Results:");
-		
 		speedFines.writeAsCsv(outputFilePath+"/speedfines.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
-		speedFines.print().setParallelism(1);
+		
+
+
+
+		// Accident Reporter
+		
+		// A map function that works on the previously filtered and windowed data calculates the start and stop time of each 4 element and returns a tuple7
+		class accidentMap implements WindowFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple7<Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple, GlobalWindow> {
+			@Override 
+			public void apply(Tuple key, 
+							  GlobalWindow gw, 
+							  Iterable<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> it,
+							  Collector<Tuple7<Integer, Integer, Integer, Integer, Integer, Integer, Integer>> c) throws Exception {	
+			
+			int count = 0;
+			Integer startTime = 0, stopTime =0;
+			List<Integer> times = new ArrayList<Integer>();
+			for(Tuple8 i : it) {
+				count += 1;
+				times.add((int)i.f0);	
+			}
+			if(count == 4){
+				startTime = Collections.min(times);
+				stopTime = Collections.max(times);
+				c.collect(new Tuple7<Integer, Integer, Integer, Integer, Integer, Integer, Integer>(startTime, stopTime, key.getField(0), key.getField(1), key.getField(2), key.getField(3), key.getField(4)));
+			}
+		}
+	}
+
+		// filter to speed 0
+		SingleOutputStreamOperator accidents = input.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
+			@Override
+			public boolean filter(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> d) throws Exception {
+				return (d.f2 == 0);
+			}
+		}).setParallelism(1)
+			.keyBy(1, 3, 6, 5, 7) // keyby in order to count the wehicles only if they are stopped in the same place
+			.countWindow(4, 1) //window of 4 elements slided by each element
+			.apply(new accidentMap()); //applyting the map function
+		
+		// Print the results for accidents
+		accidents.writeAsCsv(outputFilePath+"/accidents.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 		
 		// execute
 		try {
